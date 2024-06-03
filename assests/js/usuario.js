72036b84b2200cd342d197985fc581c2b6a9510d
bfjkgbkfjds <163414576+bfjkgbkfjds@users.noreply.github.com>
2024-06-03 00:23:26 -0400

    usuario.js

--- assests/js/usuario.js
+++ assests/js/usuario.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", function() {
     registerForm.addEventListener('submit', function(event) {
         event.preventDefault(); // Evitar el envío del formulario por defecto
 
-        // Obtener los valores del formulario
         const nombre = document.getElementById('nombre').value;
         const apellido = document.getElementById('apellido').value;
         const direccion = document.getElementById('direccion').value;
@@ -11,13 +10,11 @@ document.addEventListener("DOMContentLoaded", function() {
         const password = document.getElementById('password').value;
         const confirm_password = document.getElementById('confirm_password').value;
 
-        // Validar que las contraseñas coincidan
         if (password !== confirm_password) {
             alert('Las contraseñas no coinciden');
             return;
         }
 
-        // Construir el objeto de usuario
         const user = {
             nombre: nombre,
             apellido: apellido,
@@ -26,11 +23,7 @@ document.addEventListener("DOMContentLoaded", function() {
             password: password
         };
 
-        // Simular el envío del formulario (aquí iría tu lógica para enviar los datos al servidor)
         console.log('Registro exitoso:', user);
-        // Aquí podrías enviar los datos del usuario al servidor mediante una solicitud HTTP
-
-        // Limpiar el formulario después del registro exitoso
         registerForm.reset();
     });
 });
